7b065823734d5d9ad93d6efdc2a216fa82475c9f
DracoR22 <rdraco039@gmail.com>
2023-11-19 21:04:29 -0500

    prisma

--- components/dashboard/NavItem.tsx
+++ components/dashboard/NavItem.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { Layout, Activity, Settings, CreditCard } from "lucide-react"
 import { useRouter, usePathname } from "next/navigation"
 import { Button } from "../ui/button"
+import { Skeleton } from "../ui/skeleton"
 
 export type Organization = {
     id: string
@@ -74,6 +75,17 @@ const NavItem = ({ isExpanded, isActive, organization, onExpand }: NavItemsProps
          ))}
        </AccordionContent>
      </AccordionItem>
+  )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute"/>
+      </div>
+      <Skeleton className="h-10 w-full"/>
+    </div>
   )
 }
 
